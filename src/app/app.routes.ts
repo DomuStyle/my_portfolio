f9d7d6dfc9060ab5c8d0d6605a1725d189a2508c
DomuStyle <oxgameboyxo@googlemail.com>
2025-01-21 16:23:34 +0100

    added: about me component + html, scss & assets

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { LandingpageComponent } from './landingpage/landingpage.component';
+import { AboutmeComponent } from './aboutme/aboutme.component';
 
 export const routes: Routes = [
     {path: '', component: LandingpageComponent},
+    {path: '', component: AboutmeComponent},
 ];
