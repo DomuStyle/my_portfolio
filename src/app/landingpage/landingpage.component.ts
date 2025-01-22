fbafd881e0ce38066064a7eb106c965d8429203b
DomuStyle <oxgameboyxo@googlemail.com>
2025-01-22 14:21:30 +0100

    added: animationto arrow down on landingpage component

--- src/app/landingpage/landingpage.component.ts
+++ src/app/landingpage/landingpage.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { AboutmeComponent } from '../aboutme/aboutme.component';
+import { SkillsetComponent } from '../skillset/skillset.component';
 
 @Component({
   selector: 'app-landingpage',
   standalone: true,
-  imports: [AboutmeComponent],
+  imports: [AboutmeComponent, SkillsetComponent],
   templateUrl: './landingpage.component.html',
   styleUrl: './landingpage.component.scss'
 })
