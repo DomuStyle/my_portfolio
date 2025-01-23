fdc37bdba17c238f70cc6b392b4787bf54384a32
DomuStyle <oxgameboyxo@googlemail.com>
2025-01-23 17:22:54 +0100

    added: projects, references & contact components

--- src/app/landingpage/landingpage.component.ts
+++ src/app/landingpage/landingpage.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { AboutmeComponent } from '../aboutme/aboutme.component';
 import { SkillsetComponent } from '../skillset/skillset.component';
+import { ProjectsComponent } from '../projects/projects.component';
+import { ReferencesComponent } from '../references/references.component';
+import { ContactComponent } from '../contact/contact.component';
 
 @Component({
   selector: 'app-landingpage',
   standalone: true,
-  imports: [AboutmeComponent, SkillsetComponent],
+  imports: [AboutmeComponent, SkillsetComponent, ProjectsComponent, ReferencesComponent, ContactComponent],
   templateUrl: './landingpage.component.html',
   styleUrl: './landingpage.component.scss'
 })
