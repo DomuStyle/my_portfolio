a741312c5b01bf0da737071d5ec595517d6ba577
DomuStyle <oxgameboyxo@googlemail.com>
2025-02-02 21:04:53 +0100

    installed: ngx translation en + de

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,14 +3,24 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { ProjectdetailsComponent } from './projectdetails/projectdetails.component';
+import {
+  TranslateService,
+  TranslatePipe,
+  TranslateDirective
+} from "@ngx-translate/core";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, ProjectdetailsComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, ProjectdetailsComponent, TranslatePipe, TranslateDirective],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+  constructor(private translate: TranslateService) {
+    this.translate.addLangs(['de', 'en']);
+    this.translate.setDefaultLang('en');
+    this.translate.use('en');
+}
   title = 'portfolio';
 }
--- src/app/landingpage/landingpage.component.ts
+++ src/app/landingpage/landingpage.component.ts
@@ -4,11 +4,12 @@ import { SkillsetComponent } from '../skillset/skillset.component';
 import { ProjectsComponent } from '../projects/projects.component';
 import { ReferencesComponent } from '../references/references.component';
 import { ContactComponent } from '../contact/contact.component';
+import {TranslatePipe, TranslateDirective} from "@ngx-translate/core";
 
 @Component({
   selector: 'app-landingpage',
   standalone: true,
-  imports: [AboutmeComponent, SkillsetComponent, ProjectsComponent, ReferencesComponent, ContactComponent,],
+  imports: [AboutmeComponent, SkillsetComponent, ProjectsComponent, ReferencesComponent, ContactComponent, TranslatePipe, TranslateDirective],
   templateUrl: './landingpage.component.html',
   styleUrl: './landingpage.component.scss'
 })
