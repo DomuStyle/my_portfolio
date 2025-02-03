367c0d54fff84e8d3048f27b32a2fdc572069944
DomuStyle <oxgameboyxo@googlemail.com>
2025-02-03 15:39:54 +0100

    added: openModal method to project 1 element

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, viewChild } from '@angular/core';
+import {Overlay, OverlayModule} from '@angular/cdk/overlay';
+import { CdkPortal, PortalModule } from '@angular/cdk/portal';
+import { ProjectdetailsComponent } from '../projectdetails/projectdetails.component';
 
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [],
+  imports: [OverlayModule, PortalModule, ProjectdetailsComponent],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.scss'
 })
 export class ProjectsComponent {
+  @ViewChild(CdkPortal) portal!: CdkPortal;
 
+  constructor(private overlay: Overlay) {
+
+  }
+
+  openModal() {
+    const overlayRef = this.overlay.create();
+    overlayRef.attach(this.portal);
+  }
 }
